8a1a74b59324ae2f13715d186b37f7ef65ff5026
Shaun Husain <shaun.husain@gmail.com>
2023-04-02 19:37:05 -0500

    Having priority queue extend queue

--- src/PriorityQueue.ts
+++ src/PriorityQueue.ts
@@ -1,14 +1,9 @@
-import { LinkedList } from "./LinkedList";
 import { ListNode } from "./ListNode";
 import Person from "./Person";
+import { Queue } from "./Queue";
 
-export class PriorityQueue {
-    linkedList: LinkedList;
-  
-    constructor() {
-      this.linkedList = new LinkedList();
-    }
-  
+export class PriorityQueue extends Queue {
+
     enqueue(person: Person): boolean {
         let nodeToInsert = new ListNode(person);
 
@@ -16,33 +11,26 @@ export class PriorityQueue {
             this.linkedList.head = new ListNode(person);
             return true
         }
-        
+
         if (person.id < this.linkedList.head.person.id) {
             nodeToInsert.next = this.linkedList.head
             this.linkedList.head = nodeToInsert
             return true
         }
-        
+
         let current = this.linkedList.head;
         while (current.next !== null && current.next.person.id < nodeToInsert.person.id) {
             current = current.next;
         }
 
         // Check to see if the one we hit is equal to one about to insert
-        if(current.next && nodeToInsert.person.id == current.next.person.id) {
+        if (current.next && nodeToInsert.person.id == current.next.person.id) {
             return false;
         }
-        
+
         nodeToInsert.next = current.next
         current.next = nodeToInsert
         return true
     }
-  
-    dequeue(): Person| null {
-      if (this.linkedList.head === null) return null;
-      const person = this.linkedList.head.person;
-      this.linkedList.deleteNode(person);
-      return person;
-    }
-  }
-  
+
+}
